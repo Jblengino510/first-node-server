6e8aec8c42d2aa9359d1166c0a76e27817eb924f
Joshua <j.blengino@yahoo.com>
2022-03-06 23:05:19 -0800

    basic routing system setup

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const http = require('http')
+//module for reading files
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.method)
 
     //set header content type
-    res.setHeader('Content-Type', 'text/plain')
-    //sends content to browser
-    res.write('hello josh')
-    res.end()
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views/'
+    switch(req.url) {
+        case '/':
+            path += 'index.html'
+            break
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'
+            break
+    }
+
+    //send html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            //content of html file
+            res.end(data)
+        }
+    })
+    
 })
 
 //2nd paramater is localhost by default, you can change if you want
