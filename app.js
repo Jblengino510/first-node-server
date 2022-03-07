68afc76dc0eed361c0e1568cb20773257e115884
Joshua <j.blengino@yahoo.com>
2022-03-07 00:05:42 -0800

    basic routes, redirects, and 404 page added

--- app.js
+++ app.js
@@ -7,5 +7,20 @@ app.listen(3000)
 
 app.get('/', (req, res) => {
     //don't need to set headers or status codes
-    res.send('<h1>My First Express App!!!</h1>')
+    res.sendFile('./views/index.html', { root: __dirname})
+})
+
+app.get('/about', (req, res) => {
+    //don't need to set headers or status codes
+    res.sendFile('./views/about.html', { root: __dirname})
+})
+
+//redirects
+app.get('about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname})
 })
